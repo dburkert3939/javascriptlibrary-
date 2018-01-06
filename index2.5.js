be4d9c385f1b3f66094b22cc76b66d72e57ca929
Dustin Burkert <dustinburkert5@gmail.com>
2018-01-05 20:30:34 -0500

    javascript info console 2.5

---
+++ index2.5.js
@@ -0,0 +1,15 @@
+let n = 123;
+n = 12.345;
+// numbers are a data type (7 data types total)
+
+let name = 'Dustin';
+
+alert('Hello,' + name);
+//strings are another data type
+
+let isGreater = 4 > 1;
+alert (isGreater);
+//boolean(another data type) have only two values true and false
+
+//null, undefined, object, and symbol are the other 4 data types
+//use typeof (x) to find out which data type
