f0c567dceeae63f0119d053c017b7f7196340e4c
Dustin Burkert <dustinburkert5@gmail.com>
2018-01-07 15:27:10 -0500

    javascript info module 2.15

---
+++ index2.15.js
@@ -0,0 +1,23 @@
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+  );
+
+  //change to arrow functions 
+
+  function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+  );
